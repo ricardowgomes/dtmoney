c474528e80584e57a2bc054934b33ced4e71b299
ricardowgomes <ricardowgomes@outlook.com>
2021-09-13 18:32:13 -0600

    set state for values in the form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, FormEvent } from 'react';
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
@@ -11,7 +11,14 @@ interface Props {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: Props) {
+  const [title, setTitle] = useState('');
+  const [amount, setAmount] = useState(0);
+  const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault();
+  }
 
   return (
     <Modal
@@ -28,16 +35,20 @@ export function NewTransactionModal({ isOpen, onRequestClose }: Props) {
       >
         <img src={closeImg} alt="close modal" />
       </button>
-      <Container>
+      <Container onSubmit={handleCreateNewTransaction}>
         <h2>New Transaction</h2>
 
         <input
           placeholder='Title'
+          value={title}
+          onChange={e => setTitle(e.target.value)}
         />
 
         <input
           type="number"
           placeholder='Amount'
+          value={amount}
+          onChange={e => setAmount(+e.target.value)}
         />
 
         <Card>
@@ -63,7 +74,9 @@ export function NewTransactionModal({ isOpen, onRequestClose }: Props) {
         </Card>
 
         <input
-          placeholder='Type'
+          placeholder='Category'
+          value={category}
+          onChange={e => setCategory(e.target.value)}
         />
 
         <button type="submit">
